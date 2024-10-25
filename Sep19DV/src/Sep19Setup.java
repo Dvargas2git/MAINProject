9326e886d4ab08fe87dbe34fce46b856f3d868d3
Dvargas2git <dvargas2@stevenson.edu>
2024-10-25 18:02:58 -0400

    Update Oct7Setup.java
    This was the code for Oct7

--- Sep19DV/src/Sep19Setup.java
+++ Sep19DV/src/Sep19Setup.java
@@ -1,7 +1,21 @@
 import java.util.Scanner;
 
 public class Sep19Setup {
+    private double expenses;
+//**************
+    public Sep19Setup(double expenses) {
+        this.expenses = expenses;
+    }
 
+    public void toStringMethod() {
+        System.out.println("Expenses: " + expenses);
+    }
+
+    public double calculateNetSalary(double salary) {
+        return salary - expenses;
+    }
+//* during this section on top where the starts are i and this to do the math for the 
+//* for the system which would tgake you salary and subtract your expenses
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String fullName = "";
@@ -13,7 +27,6 @@ public class Sep19Setup {
         fullName = scanner.nextLine();
 //* During this section of the code the line 6- 13 i just identified all the variables i used for this assignemnet and i
 //*i knew i would need some string and double becaue im asking for values in numbers
-	    
         boolean validSalary = false;
         while (!validSalary) {
             System.out.print("Enter your salary: ");
@@ -31,30 +44,46 @@ public class Sep19Setup {
         System.out.print("Enter your profession: ");
         profession = scanner.nextLine();
 
-        System.out.print("Are you a homeowner? Y/N: ");
+        System.out.print("Are you a homeowner? Yes/No: ");
         String homeownerInput = scanner.nextLine();
         switch (homeownerInput.toLowerCase()) {
-            case "Y":
+            case "Yes":
                 isHome = true;
                 break;
-            case "N":
+                //*during this i wanted to update this ection a little more from just a simple answert to a more professonial 
+                //* pers ay you would have a simple Y/N when you could actualy put the words to make the interface a lot nicer
+                
+            case "No":
                 isHome = false;
                 break;
             default:
-                System.out.println("Not A homeowe");
+                System.out.println("Not a homeowner");
                 isHome = false;
                 break;
         }
 //* These next code was included due to the fact that i needed to add a swtich i had wrote the code without using it 
 //* becasue i would it to be more efficent and i also used pasted classwork for this section
+        System.out.print("Enter your monthly expenses: ");
+        double monthlyExpenses = scanner.nextDouble();
+        Sep19Setup user = new Sep19Setup(monthlyExpenses);
+        user.toStringMethod();
+
+        double netSalary = user.calculateNetSalary(salary);
         System.out.println("\n--- User Information ---");
         System.out.println("Full Name: " + fullName);
-        System.out.println("Salary: " + salary);
+        System.out.println("Salary after expenses: " + salary);
+        //* NEW CODE During this section of the code i wanted to add the salary of the person 
+        //* with this i also wanted to add the salary - the expnese so they could see what they actually earn
+        //* so that they could know how much money they have to invest which will be in another code
+        
         System.out.println("Profession: " + profession);
         System.out.println("Homeowner: " + (isHome ? "Yes" : "No"));
+        System.out.println("Net Salary after expenses: " + netSalary);
+        System.out.println("Salary: " + salary);
 //* I simply just printed all the options i wanted to bu just println and adding the amount os it can display
         scanner.close();
-	//* during this section Which is the section addition to the code we will
+        
+    	//* during this section Which is the section addition to the code we will
 	//* we will be adding a chart to compare the amount of someones salary to 
 	//* how much they are allowed to spend on the on a house by giving them a net price
 	//*$600,000 for every $200,000 they make or a conversion rate of .66
